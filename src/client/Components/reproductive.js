58ad527c8099d52c832f4830f28063241bb1f8f6
Manuel Galindez <manuelgalindez81@gmail.com>
2016-08-09 04:40:43 -0300

    Delete reproductive.js

--- src/client/Components/reproductive.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default class Reproductive extends React.Component {
-	render() {
-
-		return <iframe width="560" height="315" src="https://www.youtube.com/embed/QxsmWxxouIM" frameborder="0" allowfullscreen></iframe>
-	}
-}
